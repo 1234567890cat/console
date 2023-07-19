77db91ec785ac8408fca3f311ef40f02a5950ef2
1234567890cat <110482496+1234567890cat@users.noreply.github.com>
2023-07-19 19:51:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var a = 10;
+
+var b = 5;
+
+var add = a + b;
+
+var first_name = "Ranbir"
+
+var last_name = "Kapoor"
+
+first_name.concat(last_name);
+
+
+
+
+
+
+var add = a + b;
+console.log("hello");
